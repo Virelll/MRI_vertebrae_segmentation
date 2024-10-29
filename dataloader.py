6bbfb51e1af22389bd58d51623588e7f3a1b2a45
Virelll <72577824+Virelll@users.noreply.github.com>
2024-10-30 00:31:37 +0300

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -21,13 +21,3 @@ def createCatDataLoader(images_list, masks_list, transform, shuffle, num_classes
     res_dataset = torch.utils.data.ConcatDataset(datasets)
     dataloader = DataLoader(res_dataset, batch_size=batch_size, shuffle=shuffle)
     return dataloader
-
-# # train_dataset_320 = CustomTensorDataset(torch.from_numpy(img_train_320).type(torch.float32),torch.from_numpy(masks_train_320).type(torch.float32),count_classes_mask(masks_384),transform=train_transform)
-# train_dataset = CustomTensorDataset(torch.from_numpy(img_train_384).type(torch.float32),torch.from_numpy(masks_train_384).type(torch.float32),count_classes_mask(masks_384),transform=train_transform)
-# # train_dataset = torch.utils.data.ConcatDataset([train_dataset_320, train_dataset_384])
-# train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-#
-# # test_dataset_320 = CustomTensorDataset(torch.from_numpy(img_test_320).type(torch.float32),torch.from_numpy(masks_test_320).type(torch.float32),count_classes_mask(masks_384),transform=test_transform)
-# test_dataset = CustomTensorDataset(torch.from_numpy(img_test_384).type(torch.float32),torch.from_numpy(masks_test_384).type(torch.float32),count_classes_mask(masks_384),transform=test_transform)
-# # test_dataset = torch.utils.data.ConcatDataset([test_dataset_320, test_dataset_384])
-# test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
